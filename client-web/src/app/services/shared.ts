f3632e3470d41521769010cb40d8cb26c33c0b57
Zeljko Smiljanic <z.smiljanic@enlightitsourcing.com>
2019-04-16 10:51:41 +0200

    Small fixes

--- client-web/src/app/services/shared.ts
+++ client-web/src/app/services/shared.ts
@@ -2,9 +2,9 @@ import { Observable, of } from 'rxjs';
 
 export const API_ROOT = 'localhost:3000/API/';
 
-export function handleError<T> (operation = 'operation', result?: T) {
+export function handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
         console.error(error);
         return of(result as T);
-    }
+    };
 }
