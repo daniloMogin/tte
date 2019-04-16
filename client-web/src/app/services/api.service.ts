5ffb1f8dcf0b635af34b2bf58651dbf180355c82
Nemanja Obradovic <n.obradovic@enlightitsourcing.com>
2019-04-16 10:16:36 +0200

    api service created

--- client-web/src/app/services/api.service.ts
+++ client-web/src/app/services/api.service.ts
@@ -1,12 +1,34 @@
 import { Injectable } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+private API_ROOT = 'http://localhost:3000/api/';
 
-  constructor(private http: HttpClientModule) { }
+  constructor(private http: HttpClient) { }
 
-  
+  getGroup(): Observable<any> {
+    return this.http.get<any>(this.API_ROOT + 'group')
+  }
+
+  createGroup(group: object): Observable<any> {
+    return this.http.post<any>(this.API_ROOT + 'group', group)
+
+  }
+
+  getCupById(id): Observable<any> {
+    return this.http.get<any>(this.API_ROOT + 'group/' + id)
+  }
+
+  updateGroup(id, group: object): Observable<any> {
+    return this.http.put<any>(this.API_ROOT + 'group/' + id , group)
+  }
+
+  deleteGroup(id, group: object): Observable<any> {
+    return this.http.delete<any>(this.API_ROOT + 'group/' + id, group)
+  }
+
 }
