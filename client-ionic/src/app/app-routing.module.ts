d0e97e0aa3b76431035c6829cea1122a83bbdf10
Nemanja Obradovic <n.obradovic@enlightitsourcing.com>
2019-04-17 15:37:39 +0200

    components imported to app routing module

--- client-ionic/src/app/app-routing.module.ts
+++ client-ionic/src/app/app-routing.module.ts
@@ -1,20 +1,45 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
+import { CupsComponent } from './components/cups/cups.component';
+import { CupDetailComponent } from './components/cup-detail/cup-detail.component';
+import { GroupsComponent } from './components/groups/groups.component';
+import { GroupDetailComponent } from './components/group-detail/group-detail.component';
+import { TeamsComponent } from './components/teams/teams.component';
+import { TeamDetailComponent } from './components/team-detail/team-detail.component';
+import { GamesComponent } from './components/games/games.component';
+import { GameDetailComponent } from './components/game-detail/game-detail.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+
+// const routes: Routes = [
+//   {
+//     path: '',
+//     redirectTo: 'home',
+//     pathMatch: 'full'
+//   },
+//   {
+//     path: 'home',
+//     loadChildren: './home/home.module#HomePageModule'
+//   },
+//   {
+//     path: 'list',
+//     loadChildren: './list/list.module#ListPageModule'
+//   }
+// ];
+
 const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-  {
-    path: 'home',
-    loadChildren: './home/home.module#HomePageModule'
-  },
-  {
-    path: 'list',
-    loadChildren: './list/list.module#ListPageModule'
-  }
+  { path: 'cups', component: CupsComponent },
+  { path: 'cups/:id', component: CupDetailComponent },
+  { path: 'groups', component: GroupsComponent },
+  { path: 'groups/:id', component: GroupDetailComponent },
+  { path: 'teams', component: TeamsComponent },
+  { path: 'teams/:id', component: TeamDetailComponent },
+  { path: 'games', component: GamesComponent },
+  { path: 'games/:id', component: GameDetailComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '', redirectTo: '/cups', pathMatch: 'full' }
 ];
 
 @NgModule({
