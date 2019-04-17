a3a01d0c8938d76ecf60605bc79674b8ea1a15e3
Nemanja Obradovic <n.obradovic@enlightitsourcing.com>
2019-04-17 15:32:34 +0200

    component imported in app module

--- client-ionic/src/app/app.module.ts
+++ client-ionic/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
@@ -9,13 +10,36 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { CupsComponent } from './components/cups/cups.component';
+import { CupDetailComponent } from './components/cup-detail/cup-detail.component';
+import { GroupsComponent } from './components/groups/groups.component';
+import { GroupDetailComponent } from './components/group-detail/group-detail.component';
+import { TeamsComponent } from './components/teams/teams.component';
+import { TeamDetailComponent } from './components/team-detail/team-detail.component';
+import { GamesComponent } from './components/games/games.component';
+import { GameDetailComponent } from './components/game-detail/game-detail.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    CupsComponent,
+    CupDetailComponent,
+    GroupsComponent,
+    GroupDetailComponent,
+    TeamsComponent,
+    TeamDetailComponent,
+    GamesComponent,
+    GameDetailComponent,
+    LoginComponent,
+    RegisterComponent],
   entryComponents: [],
   imports: [
     BrowserModule,
     IonicModule.forRoot(),
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [
     StatusBar,
