691368efa31bf67dd8865d7f63a546ff9061f249
Nemanja Obradovic <n.obradovic@enlightitsourcing.com>
2019-04-19 11:28:54 +0200

    added edit modal for games

---
+++ client-ionic/src/app/components/modal/edit-modal-games/edit-modal-games.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { ModalController, NavParams } from '@ionic/angular';
+import { GamesService } from '../../../services/games.service';
+
+@Component({
+  selector: 'app-edit-modal-games',
+  templateUrl: './edit-modal-games.component.html',
+  styleUrls: ['./edit-modal-games.component.scss'],
+})
+export class EditModalGamesComponent implements OnInit {
+
+  game: any;
+
+  constructor(public modalCtrl: ModalController, private navParams: NavParams, private gameService: GamesService) { }
+
+  ngOnInit() {
+    this.game = this.navParams.data;
+    console.log(this.game);
+  }
+
+  closeModal() {
+    this.modalCtrl.dismiss();
+  }
+}
