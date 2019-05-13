0986f484b8cad3c5f8a6ebf0e0f8d2d71383927b
Zeljko Smiljanic <z.smiljanic@enlightitsourcing.com>
2019-05-13 14:03:12 +0200

    Added info table to game details page

--- client-ionic/src/app/components/game-detail/game-detail.component.ts
+++ client-ionic/src/app/components/game-detail/game-detail.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { GamesService } from '../../services';
+import { EditModalGamesComponent } from '../modal/edit-modal-games/edit-modal-games.component';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-game-detail',
@@ -11,8 +13,9 @@ export class GameDetailComponent implements OnInit {
 
   private game: any[] = [];
   showBar: boolean = true;
+  loaded = false;
 
-  constructor(private route: ActivatedRoute, private gamesService: GamesService) { }
+  constructor(private route: ActivatedRoute, private gamesService: GamesService, private modalCtrl: ModalController) { }
 
   ngOnInit() {
   }
@@ -21,9 +24,27 @@ export class GameDetailComponent implements OnInit {
     const id = this.route.snapshot.paramMap.get('id');
     this.gamesService.getGameById(id).subscribe(response => {
       this.game = response.game;
+      this.loaded = true;
       this.showBar = false;
       console.log(this.game);
     });
+  }
+
+  async openGameEditModal() {
+    const modal = await this.modalCtrl.create({
+      component: EditModalGamesComponent,
+      componentProps: this.game,
+      cssClass: 'auto-height'
+    });
+
+    modal.onDidDismiss()
+      .then((data) => {
+        if (data.data) {
+          this.game = data.data;
+        }
+      });
+
+    return await modal.present();
   }
 
 }
